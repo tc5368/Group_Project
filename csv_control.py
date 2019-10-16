e85c048818b12e532a862037ededed484cf273be
Tom Clare <tom@Toms-MacBook-Air.local>
2019-10-16 01:35:50 +0100

    Error in csv

--- csv_control.py
+++ csv_control.py
@@ -19,7 +19,12 @@ def update_current_user(profile):
 			if row[0] != profile[0]:
 				all_info.append(row)
 	user_info.close()
+	
 	all_info.append(profile)
+
+	erasing = open('This_will_be_a_database.csv','w+')
+	erasing.close()
+
 	with open('This_will_be_a_database.csv', mode='w') as user_info:
 		writer = csv.writer(user_info)
 		for i in all_info:
