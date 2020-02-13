434911b0c2b10aa554a073b66f8e4a34473ddebd
Dylan Johns <johnsd2@cardiff.ac.uk>
2020-02-13 12:38:13 +0000

    Working with Sam's database

--- LoginLogoutPrototype/shop/models.py
+++ LoginLogoutPrototype/shop/models.py
@@ -4,25 +4,7 @@ from shop import login_manager,db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-# class Company(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     company_name = db.Column(db.String(20),nullable=False)
-#     bikes = db.relationship('Bike', backref='company', lazy=True)
 
-#     def __repr__(self):
-#         return f"Company('{self.company_name}')"
-
-# class Bike(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(50),nullable=False)
-#     description = db.Column(db.String(120), nullable=False)
-#     price = db.Column(db.Numeric(10,2), nullable=False)
-#     image_file = db.Column(db.String(30), nullable=False, default='default.jpg')
-#     stock_level = db.Column(db.Integer, nullable=False)
-#     company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
-
-#     def __repr__(self):
-#         return f"Bike('{self.title}', '{self.description}', '{self.price}', '{self.stock_level}')"
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -47,16 +29,7 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-# class Checkout(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String(50),nullable=False)
-#     last_name = db.Column(db.String(120), nullable=False)
-#     card_number = db.Column(db.Numeric(20), nullable=False)
-#     valid_until = db.Column(db.String(30), nullable=False)
-#     security_code = db.Column(db.Numeric(5), nullable=False)
 
-#     def __repr__(self):
-#         return f"Checkout('{self.card_number}', '{self.valid_unitl}', '{self.security_code}')"
 
 @login_manager.user_loader
 def load_user(user_id):
