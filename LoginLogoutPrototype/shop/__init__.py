f7a8b6b9ec6d9e2df78c8844025d7dc432fafbfa
Tom Clare <tom@v1201-0a984efd.wifi.cf.ac.uk>
2020-02-18 11:20:05 +0000

    Changed to all run from same database

--- LoginLogoutPrototype/shop/__init__.py
+++ LoginLogoutPrototype/shop/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd63e56c75fc4923a027728f430dfb2c899e67a632497317a'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1769261:apmWzUswLy6LvfX@csmysql.cs.cf.ac.uk:3306/c1834256'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1769261:apmWzUswLy6LvfX@csmysql.cs.cf.ac.uk:3306/c1769261_Second_Year'
 
 
 db = SQLAlchemy(app)
