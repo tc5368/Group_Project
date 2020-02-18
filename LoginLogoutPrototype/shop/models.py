9f2d28aefb604657ecc324f7aee64153542d05b8
Dylan Johns <johnsd2@cardiffac.uk>
2020-02-18 11:42:22 +0000

    Removed username coloumn

--- LoginLogoutPrototype/shop/models.py
+++ LoginLogoutPrototype/shop/models.py
@@ -10,13 +10,12 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(20), nullable=False)
-    username = db.Column(db.String(15), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.email}')"
 
     @property
     def password(self):
