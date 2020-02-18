20af91eb589e9d28ede8d4f31723da44d6709794
tc5368 <tc536890@gmail.com>
2020-02-18 11:10:54 +0000

    Changed login credentials

--- LoginLogoutPrototype/shop/__init__.py
+++ LoginLogoutPrototype/shop/__init__.py
@@ -5,11 +5,9 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd63e56c75fc4923a027728f430dfb2c899e67a632497317a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1769261:apmWzUswLy6LvfX@csmysql.cs.cf.ac.uk:3306/c1834256'
 
 
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1834256:Chocolate64@csmysql.cs.cf.ac.uk:3306/c1834256'
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
