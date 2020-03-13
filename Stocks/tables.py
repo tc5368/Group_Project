4557f66cb165f305375f5eb52215dac4e4a7d2b8
pigu1999 <johnsd2@cardiff.ac.uk>
2020-03-13 15:57:52 +0000

    search minor changes

--- Stocks/tables.py
+++ Stocks/tables.py
@@ -4,4 +4,3 @@ class Results(Table):
     Stock_ID         = Col('Stock ID')
     Stock_Name       = Col('Stock Name')
     Current_Price    = Col('Current Price')
-    Stock_Table      = Col('Stock Table', show='False')
