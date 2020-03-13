a1cfd17286c2ee91472c660b3e63820bdf007013
Tom Clare <tc536890@gmail.com>
2020-03-13 16:11:29 +0000

    removed tables

--- Stocks/tables.py
+++
@@ -1,6 +0,0 @@
-from flask_table import Table, Col
-
-class Results(Table):
-    Stock_ID         = Col('Stock ID')
-    Stock_Name       = Col('Stock Name')
-    Current_Price    = Col('Current Price')
