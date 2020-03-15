c54aa21f03c2aa55c1368f3c2d16c49333b7f5b4
Tom Clare <tc536890@gmail.com>
2020-03-15 18:19:42 +0000

    automation

--- Stocks/routes.py
+++ Stocks/routes.py
@@ -38,7 +38,7 @@ from newsapi import NewsApiClient
 newsapi = NewsApiClient(api_key='0f58067ab2ad447ba8e4af81ecea25c5')
 
 # scheduler = BackgroundScheduler()
-# scheduler.add_job(func=new_day, trigger="cron", hour=8, minute=30)
+# scheduler.add_job(func=new_day, trigger="cron", hour=20, minute=30)
 # scheduler.start()
 new_day()
 
