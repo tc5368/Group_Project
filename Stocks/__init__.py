fce22b8b288990ba22a4cdf9f3c25aa0a97a1eae
Ruslan Levond <levondr@cardiff.ac.uk>
2020-04-08 20:25:09 +0100

    Added a txt file that explains Security features of our system and made session cookies more secure

--- Stocks/__init__.py
+++ Stocks/__init__.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd63e56c75fc4923a027728f430dfb2c899e67a632497317a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1769261:apmWzUswLy6LvfX@csmysql.cs.cf.ac.uk:3306/c1769261_Second_Year'
 
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+)
+
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
