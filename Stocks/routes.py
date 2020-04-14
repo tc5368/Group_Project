e88333df4aeffe2c4fdbf6e16f6814bc8daeca5a
Ruslan Levond <levondr@cardiff.ac.uk>
2020-04-14 22:59:37 +0100

    Created About Page

--- Stocks/routes.py
+++ Stocks/routes.py
@@ -61,6 +61,9 @@ scheduler.start()
 def home():
 	return render_template('home.html')
 
+@app.route("/about")
+def about():
+	return render_template('about.html')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
